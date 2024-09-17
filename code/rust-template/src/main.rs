fc631f11598524df12d94b6832ba2612647acf67
Akira <moallaq2@gmail>
2024-09-18 00:18:03 +0800

    1

--- code/rust-template/src/main.rs
+++ code/rust-template/src/main.rs
@@ -1,15 +1,13 @@
-#![allow(unused)]
-
 mod error;
 mod prelude;
 mod uitls;
 
-use std::fmt::format;
 use std::fs::read_dir;
 use crate::error::Error;
 use crate::prelude::*;
 
 fn main() -> Result<()> {
+
     for entry in read_dir("./").unwrap().filter_map(|e| e.ok()) {
         let entry = entry
             .path()
