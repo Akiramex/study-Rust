64b2f4edea1a4127b5e9ece1cb7a224bed166a14
AkiraHome <1113139776@qq.com>
2024-11-29 22:49:39 +0800

    feature: support DashMap

--- code/concurrency/README.md
+++ code/concurrency/README.md
@@ -2,6 +2,8 @@
 
 ### 通道 mpsc 进行线程之间的通信
 
-### 使用并发HashMap来实时收集信息
+## 使用并发HashMap来实时收集信息
 
 ### 使用RwLock替换Mutex
+
+### 使用dashmap替换hashmap
--- code/concurrency/src/metrics.rs
+++ code/concurrency/src/metrics.rs
@@ -2,48 +2,39 @@
 // 基本功能：inc/dec/snapshot
 
 use core::fmt;
-use std::{collections::HashMap, sync::{Arc, RwLock}};
-use anyhow::{anyhow, Result};
+use std::sync::Arc;
+use anyhow::Result;
+use dashmap::DashMap;
+
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    data: Arc<RwLock<HashMap<String, i64>>>,
+    data: Arc<DashMap<String, i64>>,
 }
 
 impl Metrics {
     pub fn new() -> Self {
         Metrics {
-            data: Arc::new(RwLock::new(HashMap::new())),
+            data: Arc::new(DashMap::new()),
         }
     }
 
     pub fn inc(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?; 
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter += 1;
         Ok(())
     }
 
     pub fn dec(&self, key: impl Into<String>) -> Result<()> {
-        let mut data = self.data.write().map_err(|e| anyhow!(e.to_string()))?; 
-        let counter = data.entry(key.into()).or_insert(0);
+        let mut counter = self.data.entry(key.into()).or_insert(0);
         *counter -= 1;
         Ok(())
-    }
-
-    pub fn snapshot(&self) -> Result<HashMap<String, i64>> {
-        Ok(self
-            .data
-            .read()
-            .map_err(|e| anyhow!(e.to_string()))?
-            .clone())
     }
 }
 
 impl fmt::Display for Metrics {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let data = self.data.read().unwrap();
-        for (key , value) in data.iter() {
-            writeln!(f, "{}: {}", key, value)?
+        for entry in self.data.iter() {
+            writeln!(f, "{}: {}", entry.key(), entry.value())?
         }
         Ok(())
     }
