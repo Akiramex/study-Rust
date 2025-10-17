e341bf7071c69f1c969420c0418c02947b8a04ff
Akira <1113139776@qq.com>
2025-10-17 14:09:14 +0800

    init

--- code/wasm/wasm-game/src/lib.rs
+++ code/wasm/wasm-game/src/lib.rs
@@ -3,6 +3,8 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
+
 #[wasm_bindgen]
 pub fn add(left: i32, right: i32) -> i32 {
     left + right
@@ -11,4 +13,10 @@ pub fn add(left: i32, right: i32) -> i32 {
 #[wasm_bindgen]
 pub fn sub(left: i32, right: i32) -> i32 {
     left - right
+}
+
+#[wasm_bindgen(start)]          // 包加载时自动执行
+pub fn start() {
+    console_error_panic_hook::set_once();
+    tracing_wasm::set_as_global_default();
 }
---
+++ code/wasm/wasm-game/www/src/components/SnakeWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="content-warpper">
+        <canvas id="snake-world-canvas">
+
+        </canvas>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
